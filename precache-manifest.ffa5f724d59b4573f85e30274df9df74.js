188799e59a4cac62209ac3d5008d9c62aadcf0ad
Calvin <calvin@Calvins-MacBook-Pro.local>
2020-05-02 11:13:15 +0930

    Updates

--- precache-manifest.ffa5f724d59b4573f85e30274df9df74.js
+++ precache-manifest.ffa5f724d59b4573f85e30274df9df74.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef185e0e08aa26468e0379c56d924dfe",
+    "revision": "4f7ba8e9b10f80087b13d69065923032",
     "url": "/orienteering/index.html"
   },
   {
@@ -184,7 +184,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/orienteering/static/css/9.a089da3f.chunk.css"
   },
   {
-    "revision": "7f275b33fbe1cddfd645",
+    "revision": "750e4565a90b84715a98",
     "url": "/orienteering/static/css/main.6fd5aaf3.chunk.css"
   },
   {
@@ -524,8 +524,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/orienteering/static/js/9.7585e850.chunk.js"
   },
   {
-    "revision": "7f275b33fbe1cddfd645",
-    "url": "/orienteering/static/js/main.8a639ba5.chunk.js"
+    "revision": "750e4565a90b84715a98",
+    "url": "/orienteering/static/js/main.085ff837.chunk.js"
   },
   {
     "revision": "1d5729b853f6a22ffe13",
